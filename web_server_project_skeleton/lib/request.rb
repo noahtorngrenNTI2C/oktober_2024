1df13f16d67885a80953d49b5dee37a523e01210
noahtorngrenNTI2C <noah.torngren@elev.ga.ntig.se>
2024-11-11 09:00:56 +0100

    updete

--- web_server_project_skeleton/lib/request.rb
+++ web_server_project_skeleton/lib/request.rb
@@ -1,9 +1,39 @@
-class Request
+  class Request
+    def initialize(request_string)
+      @request_string_split = request_string.split("\n")
+      @first_line = request_string_split[0]
   
-  attr_reader :method, :resource
-
-  def initialize(request_string)
-    @method = :get
-    @resource = "/"
+    end
+  
+    def method()
+      method = @first_line.split(" ")[0]
+  
+    end
+  
+    def resource()
+      resource = @first_line[0].split(" ")[1]
+  
+    end
+  
+    def version()
+      version = @first_line.split(" ")[2]
+  
+    end
+  
+    def headers()
+      temp = @request_string_split.dup
+      temp.delete_at(0)
+      
+      headers = Hash.new
+      temp2 = temp[0].split
+      headers['Host'] = temp2.split(" ")[0]
+      headers['Accept-Language'] = temp2.split(" ")[1]
+      headers
+    end
+  
+    def params()
+      temp = @request_string_split[4]
+      
+    end
   end
-end
+  
